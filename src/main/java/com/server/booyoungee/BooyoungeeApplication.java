ba805c50fb6c679c4f35caa6279a4954c6d31645
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-06-26 16:15:56 +0900

    feat: initial commit

---
+++ src/main/java/com/server/booyoungee/BooyoungeeApplication.java
@@ -0,0 +1,13 @@
+package com.server.booyoungee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooyoungeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooyoungeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/booyoungee/BooyoungeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.booyoungee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooyoungeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
