8c25d64f885e60784fc3edc6e869b4e7ff7cc08a
younghak9905 <yeonghak9905@kyonggi.ac.kr>
2024-07-08 19:53:56 +0900

    웹훅 테스트

---
+++ src/main/java/com/server/booyoungee/domain/topSceret/api/WebhookController.java
@@ -0,0 +1,33 @@
+package com.server.booyoungee.domain.topSceret.api;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/webhook")
+@RequiredArgsConstructor
+public class WebhookController {
+
+	@Value("${movies.api.key}")
+	private String verifyTokens;
+
+	@GetMapping("")
+	public String verifyWebhook(@RequestParam("hub.mode") String mode,
+		@RequestParam("hub.verify_token") String verifyToken,
+		@RequestParam("hub.challenge") String challenge) {
+		// Verify the webhook
+		String VERIFY_TOKEN = verifyTokens;
+		System.out.print("hello world");
+		if (mode.equals("subscribe") && verifyToken.equals(VERIFY_TOKEN)) {
+			System.out.print("hello world");
+			return challenge;
+		} else {
+			return "Verification failed";
+		}
+	}
+}
