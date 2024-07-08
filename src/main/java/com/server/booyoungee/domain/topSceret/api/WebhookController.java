c1381cb2df0ed19540d840c1f9f4dfeee5963997
younghak9905 <yeonghak9905@kyonggi.ac.kr>
2024-07-08 20:07:43 +0900

    웹훅 테스트

--- src/main/java/com/server/booyoungee/domain/topSceret/api/WebhookController.java
+++ src/main/java/com/server/booyoungee/domain/topSceret/api/WebhookController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/webhook")
+@RequestMapping("/api/webhook")
 @RequiredArgsConstructor
 public class WebhookController {
 
