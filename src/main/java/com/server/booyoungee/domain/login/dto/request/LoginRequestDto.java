eabd73422e345f3028e9fa70b88fa1d8f16afeff
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-08-18 00:49:03 +0900

    [TC-30]-refactor : login dto 개행 추가

--- src/main/java/com/server/booyoungee/domain/login/dto/request/LoginRequestDto.java
+++ src/main/java/com/server/booyoungee/domain/login/dto/request/LoginRequestDto.java
@@ -4,5 +4,8 @@ import com.server.booyoungee.domain.login.domain.enums.Provider;
 
 import jakarta.validation.constraints.NotNull;
 
-public record LoginRequestDto(@NotNull Provider provider, String name) {
+public record LoginRequestDto(
+	@NotNull Provider provider,
+	String name
+) {
 }
