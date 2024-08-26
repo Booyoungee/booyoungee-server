5b1801cedfb1d3b417ca2c253808e1879eb4944e
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-08-26 19:32:49 +0900

    [TC-16]-refactor: 불필요한 import 제거

--- src/main/java/com/server/booyoungee/domain/review/comment/domain/Comment.java
+++ src/main/java/com/server/booyoungee/domain/review/comment/domain/Comment.java
@@ -22,7 +22,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OrderColumn;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
