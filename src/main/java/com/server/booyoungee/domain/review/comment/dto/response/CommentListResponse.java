7e61fa4b0019b23cd823f15c4d991416f6c9fcc1
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-09-11 02:00:57 +0900

    [TC-77]-refactor: List dto에 평균 별점 추가

--- src/main/java/com/server/booyoungee/domain/review/comment/dto/response/CommentListResponse.java
+++ src/main/java/com/server/booyoungee/domain/review/comment/dto/response/CommentListResponse.java
@@ -11,16 +11,23 @@ import io.swagger.v3.oas.annotations.media.Schema;
 public record CommentListResponse(
 	@Schema(
 		description = "리뷰 코멘트 목록",
-		example = "[{\"id\":1,\"placeId\":\"1\",\"placeName\":\"부산역\"\"content\":\"좋아요\",\"stars\":5,\"writerId\":\"1\",\"writerName\":\"홍길동\",\"createdAt\":\"2021.09.01.12:00:00\",\"updatedAt\":\"2021.09.01.12:00:00\"}]",
+		example = "[{\"id\":1,\"placeId\":\"1\",\"placeName\":\"부산역\"\"content\":\"좋아요\",\"stars\":4.5,\"writerId\":\"1\",\"writerName\":\"홍길동\",\"createdAt\":\"2021.09.01.12:00:00\",\"updatedAt\":\"2021.09.01.12:00:00\"}]",
 		requiredMode = REQUIRED
 	)
-	List<CommentResponse> contents
+	List<CommentResponse> contents,
+
+	@Schema(description = "평균 별점", example = "4.5", requiredMode = REQUIRED)
+	double averageStars
 ) {
 	public static CommentListResponse from(List<Comment> contents) {
 		return new CommentListResponse(
 			contents.stream()
 				.map(CommentResponse::from)
-				.toList()
+				.toList(),
+			contents.stream()
+				.mapToDouble(comment -> comment.getStars().getStars())
+				.average()
+				.orElse(0.0)
 		);
 	}
 }
