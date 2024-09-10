58c9db9bb49cca25d49ada29e11e723e3a998779
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-09-11 01:56:25 +0900

    [TC-77]-refactor: dto에 장소 이름 추가

--- src/main/java/com/server/booyoungee/domain/review/comment/dto/response/CommentListResponse.java
+++ src/main/java/com/server/booyoungee/domain/review/comment/dto/response/CommentListResponse.java
@@ -11,7 +11,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 public record CommentListResponse(
 	@Schema(
 		description = "리뷰 코멘트 목록",
-		example = "[{\"id\":1,\"placeId\":\"1\",\"content\":\"좋아요\",\"stars\":5,\"writerId\":\"1\",\"writerName\":\"홍길동\",\"createdAt\":\"2021.09.01.12:00:00\",\"updatedAt\":\"2021.09.01.12:00:00\"}]",
+		example = "[{\"id\":1,\"placeId\":\"1\",\"placeName\":\"부산역\"\"content\":\"좋아요\",\"stars\":5,\"writerId\":\"1\",\"writerName\":\"홍길동\",\"createdAt\":\"2021.09.01.12:00:00\",\"updatedAt\":\"2021.09.01.12:00:00\"}]",
 		requiredMode = REQUIRED
 	)
 	List<CommentResponse> contents
